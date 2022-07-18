60eadb574080c6382bbb53043801ed798183d15a
Jacob Wheelock <jacobwheelock@wustl.edu>
2022-07-18 17:28:00 -0500

    More Buttons

--- about/app.js
+++ about/app.js
@@ -1,5 +1,13 @@
 'use strict';
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function()) {
-	window.location.href="test.html";
+function aboutFunc() {
+	window.location.href="jacobwheelock.com";
+}
+function aboutFunc() {
+	window.location.href="jacobwheelock.com/about";
+}
+function pubFunc() {
+	window.location.href="jacobwheelock.com/publications";
+}
+function expFunc() {
+	window.location.href="jacobwheelock.com/experience";
 }
